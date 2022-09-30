fed10f232b1b7ab91bde769bbf3ddda811ea3740
Amartya Raj Saha <amartya@wednesday.is>
2022-09-30 17:44:47 +0530

    feat: route

--- app/routeConfig.js
+++ app/routeConfig.js
@@ -1,10 +1,15 @@
 import NotFound from '@containers/NotFoundPage/Loadable';
 import HomeContainer from '@containers/HomeContainer/Loadable';
 import routeConstants from '@utils/routeConstants';
+import SongContainer from '@containers/SongContainer/Loadable';
 export const routeConfig = {
   repos: {
     component: HomeContainer,
     ...routeConstants.repos
+  },
+  ituneSongPage: {
+    component: SongContainer,
+    ...routeConstants.songs
   },
   notFoundPage: {
     component: NotFound,
