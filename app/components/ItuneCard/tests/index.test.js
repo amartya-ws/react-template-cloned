2770b2693a67167d73e48f9468f0510bad2f91de
Amartya Raj Saha <amartya@wednesday.is>
2022-10-06 18:56:28 +0530

    add test

--- app/components/ItuneCard/tests/index.test.js
+++ app/components/ItuneCard/tests/index.test.js
@@ -5,7 +5,8 @@
  */
 
 import React from 'react';
-import { renderWithIntl } from '@utils/testUtils';
+import { fireEvent } from '@testing-library/dom';
+import { renderWithIntl, timeout } from '@utils/testUtils';
 import ItuneCard from '../index';
 
 describe('<ItuneCard />', () => {
@@ -23,17 +24,28 @@ describe('<ItuneCard />', () => {
     const data = {
       artistName: 'eminem',
       trackName: 'Till I Collapse (feat. Nate Dogg)',
-      url: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/b4/13/e9/b413e91d-40aa-a1a8-b8e3-2bacc3b3e222/00606949329020.rgb.jpg/100x100bb.jpg'
+      url: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/b4/13/e9/b413e91d-40aa-a1a8-b8e3-2bacc3b3e222/00606949329020.rgb.jpg/100x100bb.jpg',
+      audioUrl: 'https://preview.demo.com/bjgf45tf'
     };
 
-    const { artistName, trackName, url } = data;
+    const { artistName, trackName, url, audioUrl } = data;
 
     const { getByTestId } = renderWithIntl(
-      <ItuneCard artistName={artistName} trackName={trackName} artworkUrl100={url} />
+      <ItuneCard artistName={artistName} trackName={trackName} artworkUrl100={url} previewUrl={audioUrl} />
     );
 
     expect(getByTestId('song-detail')).toHaveTextContent(artistName);
     expect(getByTestId('song-detail')).toHaveTextContent(trackName);
     expect(getByTestId('song-image')).toHaveAttribute('src', url);
+    expect(getByTestId('preview-audio')).toHaveAttribute('src', audioUrl);
+  });
+
+  it('should call setCurrentSongId on play event', async () => {
+    let submitSpy = jest.fn();
+
+    const { getByTestId } = renderWithIntl(<ItuneCard setCurrentSongId={submitSpy} />);
+    fireEvent.play(getByTestId('preview-audio'));
+    await timeout(500);
+    expect(submitSpy).toBeCalled();
   });
 });
