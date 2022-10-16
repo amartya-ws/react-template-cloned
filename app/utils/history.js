d044277be6f0dd8257c2f78aec07d4bf71fecab8
Amartya Raj Saha <amartya@wednesday.is>
2022-10-16 22:48:50 +0530

    Revert "fix: uat"
    This reverts commit 5a0a1617015ac41ad4ffc34f5281cf8831648059.

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,6 +1,6 @@
 import { createBrowserHistory } from 'history';
 import { isProd, isUAT } from './index';
-//import routeConstants from '@utils/routeConstants';
+import routeConstants from '@utils/routeConstants';
 
 export function getBaseName() {
   if (isProd()) {
@@ -14,22 +14,22 @@ export function getBaseName() {
   return '';
 }
 
-// const routes = Object.keys(routeConstants);
-// const pathname = window.location.pathname;
-// let baseUrl = '';
-// if (process.env.ENVIRONMENT_NAME === 'uat') {
-//   routes.forEach((routeKey) => {
-//     const route = routeConstants[routeKey].route;
-//     if (pathname.includes(route)) {
-//       if (pathname.substring(pathname.length - route.length, pathname.length) === route) {
-//         baseUrl = pathname.substring(0, pathname.length - route.length);
-//       }
-//       if (pathname.substring(pathname.length - route.length, pathname.length - 1) === `${route}/`) {
-//         baseUrl = pathname.substring(0, pathname.length - route.length - 1);
-//       }
-//     }
-//   });
-// }
+const routes = Object.keys(routeConstants);
+const pathname = window.location.pathname;
+let baseUrl = '';
+if (process.env.ENVIRONMENT_NAME === 'uat') {
+  routes.forEach((routeKey) => {
+    const route = routeConstants[routeKey].route;
+    if (pathname.includes(route)) {
+      if (pathname.substring(pathname.length - route.length, pathname.length) === route) {
+        baseUrl = pathname.substring(0, pathname.length - route.length);
+      }
+      if (pathname.substring(pathname.length - route.length, pathname.length - 1) === `${route}/`) {
+        baseUrl = pathname.substring(0, pathname.length - route.length - 1);
+      }
+    }
+  });
+}
 
-const history = createBrowserHistory({ basename: getBaseName() });
+const history = createBrowserHistory({ basename: baseUrl });
 export default history;
